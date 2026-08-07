5fabdf1fe143ed67dd70abb0679c1a6904778f9e
Dan <Danielzareimain@gmail.com>
2026-08-07 12:43:43 +0330

    functions and const basics learned asap.

--- taskopedia/src/main.jsx
+++ taskopedia/src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+const root = createRoot(document.getElementById('root'));
 
-createRoot(document.getElementById('root')).render(
-  <div>
-    <h1>Dorood bar shoma!</h1>
-    <div>Hello, World!</div>
-  </div>
+function HomePage() {
+  return (
+    <div>
+      <h1>Dorood bar shoma!</h1>
+      <div>Hello, World!</div>
+    </div>
+  );
+}
+
+root.render(
+  HomePage()
 )
 
 
