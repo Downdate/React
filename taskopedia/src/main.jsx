c7a4b94bf8f2a89e5e4eab586ec26f1384bc9229
Dan <Danielzareimain@gmail.com>
2026-08-07 12:39:05 +0330

    added Elements to the root

--- taskopedia/src/main.jsx
+++ taskopedia/src/main.jsx
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
   <div>
-    Hello, World!
+    <h1>Dorood bar shoma!</h1>
+    <div>Hello, World!</div>
   </div>
 )
+
+
