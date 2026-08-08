df362787726bc2b67a7e2af35d9107590537415c
Dan <Danielzareimain@gmail.com>
2026-08-08 14:37:40 +0330

    added style.css to the jsx file and added Bootstrap to the index.html

--- taskopedia/src/main.jsx
+++ taskopedia/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./style.css";
 
 const root = createRoot(document.getElementById("root"));
 
@@ -15,7 +16,11 @@ function HomePage() {
 function Header() {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>React Application Header</h1>
+      <h1
+        className= "customHeading"
+      >
+        React Application Header
+      </h1>
     </div>
   );
 }
