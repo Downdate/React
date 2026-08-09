085c66bdd35b6103b1aa69c65daaee0442f96c0e
Dan <Danielzareimain@gmail.com>
2026-08-09 12:25:13 +0330

    Added Counting functionality

--- CountoPedia/src/Counter.jsx
+++ CountoPedia/src/Counter.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+
 function Counter() {
-  let count = 0;
+  let [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(function (prev) {
+      return prev + 1;
+    });
+  }
+
+  function decrement() {
+    setCount(function (prev) {
+      return prev - 1;
+    });
+  }
+
   return (
     <div className="row text-white container">
       <h1>Counter</h1>
-      <p>Count: {count}</p>
-      <button className="btn btn-success m-2">Increment (+1)</button>
-      <button className="btn btn-danger m-2">Decrement (-1)</button>
+      <h5>Count: {count}</h5>
+      <button className="btn btn-success m-2" onClick={increment}>
+        Increment (+1)
+      </button>
+      <button className="btn btn-danger m-2" onClick={decrement}>
+        Decrement (-1)
+      </button>
     </div>
   );
 }
