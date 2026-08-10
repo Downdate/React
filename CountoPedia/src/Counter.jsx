c452c0fffde3e47353f160dad4554a72be12ed0b
Dan <Danielzareimain@gmail.com>
2026-08-10 10:52:32 +0330

    added useEffect to make the victory condition

--- CountoPedia/src/Counter.jsx
+++ CountoPedia/src/Counter.jsx
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Counter() {
   let [hpPlayer, setHpPlayer] = useState(100);
   let [hpEnemy, setHpEnemy] = useState(100);
   let [lastDamageDealt, setLDD] = useState(0);
   let [lastDamageTaken, setLDT] = useState(0);
+  let [gameStatus, setGameStatus] = useState("");
 
   const playerDamage = 10;
   const playerRegen = 5;
   const enemyDamage = 7;
   const enemyRegen = 5;
 
+  useEffect(() => {
+    if (hpEnemy <= 0) {
+      setGameStatus("You killed the enemy, you won!");
+    } else if (hpPlayer <= 0) {
+      setGameStatus("You died, you lost!");
+    } else {
+      setGameStatus("");
+    }
+  }, [hpEnemy, hpPlayer]);
   function damageCalCulator(baseDamage) {
     const randomFactor = Math.random() * 0.5; //create a random factor between 0 and 0.5
     let damageAfterRandomFactor = 0;
@@ -45,22 +55,31 @@ function Counter() {
   }
 
   function playerAttack() {
+    const playerDamageDone = damageCalCulator(playerDamage);
+    const enemyDamageDone = damageCalCulator(enemyDamage);
+
+    setLDD(playerDamageDone);
+    setLDT(enemyDamageDone);
+
     setHpEnemy(function (prev) {
-      return prev - damageCalCulator(playerDamage);
+      return prev - playerDamageDone;
     });
     setHpPlayer(function (prev) {
-      return prev - damageCalCulator(enemyDamage);
+      return prev - enemyDamageDone;
     });
   }
 
   return (
     <div className="row text-white container">
       <h1>Game</h1>
+      <h3>
+        status: <p>{gameStatus}</p>
+      </h3>
       <h5>Enemy HP : {hpEnemy}</h5>
       <h5>Your HP : {hpPlayer}</h5>
       <h6>Your Damage : {playerDamage}</h6>
       <h6>Last Damage Dealt : {lastDamageDealt}</h6>
-      <h6>Last Damage Taken : {lastDamageDealt}</h6>
+      <h6>Last Damage Taken : {lastDamageTaken}</h6>
 
       <button className="btn btn-success m-2" onClick={() => heal()}>
         Heal
