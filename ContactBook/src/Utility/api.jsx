69b6b9bdf9ea407ce5a540f29080266c1a5f390e
Dan <Danielzareimain@gmail.com>
2026-08-15 19:31:15 +0330

    added api utility for getting random user data from an api

---
+++ ContactBook/src/Utility/api.jsx
@@ -0,0 +1,14 @@
+const getRandomUser = async () => {
+  const url = new URL("https://randomuser.me/api/");
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+export default getRandomUser;
