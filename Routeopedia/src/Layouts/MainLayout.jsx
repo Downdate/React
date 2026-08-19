f92e6da55b0f83a526d4a4b45f2a369dfd9f0434
Dan <Danielzareimain@gmail.com>
2026-08-19 12:22:41 +0330

    added Link from react-router-dom to make the page dynamic

--- Routeopedia/src/Layouts/MainLayout.jsx
+++ Routeopedia/src/Layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
 import reactLogo from "../assets/react.svg";
+import { Link } from "react-router-dom";
 
 function MainLayout() {
   return (
@@ -21,14 +22,14 @@ function MainLayout() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav d-flex">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/Contact">
+              <Link className="nav-link" to="/Contact">
                 Contact
-              </a>
+              </Link>
             </li>
             <li className="nav-item flex-grow-1">
               <a className="nav-link" href="#">
