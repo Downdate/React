08f3c4f2882cc9f4dbfd8210cc98a3859f16d05f
Dan <Danielzareimain@gmail.com>
2026-08-30 12:41:50 +0330

    structured the redux folder and added actions

--- ReduxoPedia/src/redux/Store.js
+++ ReduxoPedia/src/redux/Store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { counterReducer } from "./slice/CounterSlice";
-
+import { counterReducer } from "./slice/counterSlice";
+import { destinationReducer } from "./slice/destinationSlice";
 export const store = configureStore({
   reducer: {
     counterStore: counterReducer,
+    destinationStore: destinationReducer,
   },
 });
