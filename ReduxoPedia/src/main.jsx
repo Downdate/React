3ad8e6bf974ab458be877c81c6febb38fa6522f5
Dan <Danielzareimain@gmail.com>
2026-08-30 09:00:06 +0330

    added store to the project

--- ReduxoPedia/src/main.jsx
+++ ReduxoPedia/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Header from "../src/Layouts/Header";
 import Footer from "../src/Layouts/Footer";
+import { store } from "./store/Store";
+import { Provider } from "react-redux";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Provider store={store}></Provider>
     <div className="d-flex flex-column min-vh-100">
       <Header />
 
